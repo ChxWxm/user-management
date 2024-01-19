39c8982d70f9a2af1af113ae0df13f2494144b8d
wcho <chotikawimaro@Gam-Macbook.local>
2024-01-20 01:19:10 +0700

    feat: done chapter10 configuration

--- src/main/java/com/usermangement/wallet/WalletService.java
+++ src/main/java/com/usermangement/wallet/WalletService.java
@@ -3,6 +3,8 @@ package com.usermangement.wallet;
 import com.usermangement.exception.DuplicationException;
 import com.usermangement.exception.InternalServiceException;
 import com.usermangement.exception.NotFoundException;
+import com.usermangement.mail.MailService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,14 +13,20 @@ import java.util.Optional;
 
 @Service
 public class WalletService {
+
+    private MailService mailService;
+
+    public WalletService(@Qualifier("googleMailService") MailService mailService) {
+        this.mailService = mailService;
+    }
+
     private final List<Wallet> wallets = new ArrayList<>(List.of(
             new Wallet(1, "Jack", "jack@gmail.com"),
-            new Wallet(2, "Alice", "alice@gmail.com")
-    ));
+            new Wallet(2, "Alice", "alice@gmail.com")));
 
     List<Wallet> getWallets(Optional<String> name) {
         try {
-            //  callNormalService();
+            // callNormalService();
             if (name.isPresent()) {
                 return wallets.stream().filter(w -> w.getName().equals(name.get())).toList();
             }
@@ -29,8 +37,9 @@ public class WalletService {
     }
 
     Wallet createWallet(WalletRequest request) {
-        //  This is the way to use lamda method
-        //  Optional<Integer> maxId = wallets.stream().map(Wallet::getId).max(Integer::compareTo);
+        // This is the way to use lamda method
+        // Optional<Integer> maxId =
+        // wallets.stream().map(Wallet::getId).max(Integer::compareTo);
         System.out.println("email: " + request.email());
         Optional<Wallet> duplicateWallet = wallets.stream()
                 .filter(wallet -> wallet.getEmail().equals(request.email()))
@@ -43,11 +52,14 @@ public class WalletService {
         Integer currentId = maxId.orElse(0) + 1;
         Wallet wallet = new Wallet(currentId, request.name(), request.email());
         wallets.add(wallet);
+
+        mailService.sendMail("admin@wallet.com", "Wallet has created.");
         return wallet;
     }
 
     Wallet getWalletById(Integer id) {
-        return wallets.stream().filter(w -> w.getId() == id).findFirst().orElseThrow(() -> new NotFoundException("Wallet Not found by id " + id));
+        return wallets.stream().filter(w -> w.getId() == id).findFirst()
+                .orElseThrow(() -> new NotFoundException("Wallet Not found by id " + id));
     }
 
     private void callNormalService() {
